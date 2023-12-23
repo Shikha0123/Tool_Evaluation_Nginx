6bce815b6cfc21c2762292487d43195e8e532a35
Shikha Tripathi <140466237+Shikha0123@users.noreply.github.com>
2023-12-23 11:30:25 +0530

    Update backend.tf

--- Nginx/terraform_script/backend.tf
+++ Nginx/terraform_script/backend.tf
@@ -1,1 +1,17 @@
+terraform {
 
+  backend "s3" {
+
+    bucket         = "shuklaji"
+
+    key            = "Nginx/terraform.tfstate"
+
+    region         = "us-east-1"
+
+    dynamodb_table = "shuklaji"
+
+    encrypt        = true
+
+  }
+
+}
